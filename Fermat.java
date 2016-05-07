8cc5b4d7b643dd64c2773b7949c30173da657fd6
Adam Beck <akb93@case.edu>
2016-05-07 04:08:20 -0400

    Estimates fermat's theorem successfully and correctly reads and writes all input and output

--- Fermat.java
+++ Fermat.java
@@ -1,6 +1,5 @@
 /**
- * This program computes the closest numbers to satisfy
- * Fermat's Last Theorem.
+ * This program computes the closest numbers to satisfy Fermat's Last Theorem.
  * 
  * https://en.wikipedia.org/wiki/Fermat%27s_Last_Theorem
  * 
@@ -12,31 +11,36 @@
 
 //TODO: Add functionality for any power of 'n' in the future.
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Fermat {
-	public static void main(String args[]){
+	public static void main(String args[]) throws IOException{
 		String inputFileName = args[0];
 		String outputFileName = args[1];
 		
 		String[] initialConditions = scanFile(inputFileName, outputFileName);
 		
-		int a, b, c;
+		double a, b, c;
 		double ratio; // Represents the best ratio found so far.
 		
 		/* Save and print out initial conditions */
-		a = Integer.parseInt(initialConditions[0]);
-		b = Integer.parseInt(initialConditions[1]);
-		c = Integer.parseInt(initialConditions[2]);
+		a = Double.parseDouble(initialConditions[0]);
+		b = Double.parseDouble(initialConditions[1]);
+		c = Double.parseDouble(initialConditions[2]);
 		ratio = Double.parseDouble((initialConditions[3]));
 		
-		System.out.printf("Initial conditions: a=%d b=%d c=%d ratio=%f\n", a, b, c, ratio);
+		System.out.printf("Initial conditions: a=%f b=%f c=%f ratio=%f\n", a, b, c, ratio);
 		System.out.println("Program starting. Better conditions will be printed if found.");
 		
 		/* Run the program */
 		run(a, b, c, ratio, inputFileName, outputFileName);
+
 	}
 	
 	/**
@@ -59,7 +63,6 @@ public class Fermat {
 				scan.next(),
 				scan.next(),
 				scan2.next(),
-
 		};
 	}
 	
@@ -73,8 +76,55 @@ public class Fermat {
 	 * @param ratio
 	 * @param intputName
 	 * @param outputName
+	 * @throws IOException 
 	 */
-	public static void run(int a, int b, int c, double ratio, String intputName, String outputName){
+	public static void run(double a, double b, double c, double ratio, String inputName, String outputName) throws IOException{
 		
+		while(true){
+			for (; ; b++){
+				for (; ; c++){
+					//System.out.println("a=" +a+ "b=" +b+ "c=" +c);
+					/* Update conditions if neccessary */
+					if (Math.abs(1 - (double)c*c*c / (double)(a*a*a + b*b*b)) <
+							Math.abs((double)1 - ratio)){
+						
+						/* Delete old file, update with new values */
+						try {
+							 BufferedWriter writer = new BufferedWriter(new FileWriter(inputName));
+							 writer.write(a + "\n");
+							 writer.write(b + "\n");
+							 writer.write(c + "\n");
+							 writer.close();
+							 
+							 ratio = (double)c*c*c / (double)(a*a*a + b*b*b);
+							 writer = new BufferedWriter(new FileWriter(outputName));
+							 writer.write(ratio + "");
+							 writer.close();
+							 
+						} catch (FileNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						System.out.printf("New conditions: a=%f b=%f c=%f ratio=%f\n", a, b, c, ratio);
+					}
+					
+					if (a*a*a + b*b*b < c*c*c){
+						break;
+					}
+					
+				}
+				
+				while (a*a*a + b*b*b < c*c*c){
+					a++;
+				}
+			}
+			
+		}
+	}
+	
+	public static double getRatio(String outputName) throws FileNotFoundException{
+		Scanner scan2 = new Scanner(new File(outputName));
+		return Double.parseDouble(scan2.next());
 	}
 }
